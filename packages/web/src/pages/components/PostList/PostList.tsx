00f1aee8929122e345172c764ba68e15a78b9930
GreenTeaCake <167279+GreenTeaCake@users.noreply.github.com>
2024-04-08 19:56:50 +0300

    feat: Make posts selectable

--- packages/web/src/pages/components/PostList/PostList.tsx
+++ packages/web/src/pages/components/PostList/PostList.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react';
+import { useState, type FC } from 'react';
 import { getUsersMap, useAppSelector } from 'store';
 import Stack from '@mui/material/Stack';
 import { useSelector } from 'react-redux';
@@ -8,12 +8,22 @@ export const PostList: FC = () => {
   const posts = useAppSelector((state) => state.posts.value);
   const usersMap = useSelector(getUsersMap);
 
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+
   return (
     <Stack spacing={2}>
       {posts.map((post) => {
         const { id, userId } = post;
         const user = usersMap.get(userId);
-        return <PostListItem key={id} post={post} user={user} />;
+        return (
+          <PostListItem
+            isSelected={id === selectedId}
+            key={id}
+            onClick={setSelectedId}
+            post={post}
+            user={user}
+          />
+        );
       })}
     </Stack>
   );
